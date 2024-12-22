1ac5b82d8640125e65478d3e4432696433f8853d
Aditya Dani <danirama282@gmail.com>
2024-12-22 21:10:36 +0700

    adding alert in list product page when data product and data dealers is empty

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -130,7 +130,15 @@
                             // Jika tidak ada produk, tampilkan pesan
                             if (response.data.length === 0) {
                                 productContainer.html(
-                                    `<div class="text-center">${response.message}</div>`
+                                    // `<div class="text-center">${response.message}</div>`
+                                    `
+                                    <div class="col-md-12 col-lg-12 mt-4 mb-4">
+                                        <div class="alert alert-warning border-0 d-flex align-items-center" role="alert">
+                                            <div class="bg-warning me-3 icon-item"><span class="fas fa-exclamation-circle text-white fs-6"></span></div>
+                                            <p class="mb-0 flex-1">${response.message}</p><button class="btn-close" type="button" data-bs-dismiss="alert" aria-label="Close"></button>
+                                        </div>
+                                        </div>
+                                    `
                                 );
                                 return;
                             }
@@ -221,7 +229,7 @@
                     };
                 }
 
-                $('#apply-filter').click(function (e) {
+                $('#apply-filter').click(function(e) {
                     e.preventDefault();
                     loadProducts(1);
                 });
